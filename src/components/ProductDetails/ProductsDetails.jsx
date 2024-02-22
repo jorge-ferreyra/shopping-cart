0b4f35643898f358a33564cf3f25b1e08e32a50c
Jorge Ferreyra <jorxfer@gmail.com>
2024-02-22 16:14:06 -0300

    Fixing styles

--- src/components/ProductDetails/ProductsDetails.jsx
+++ src/components/ProductDetails/ProductsDetails.jsx
@@ -7,13 +7,16 @@ import { useEffect } from 'react'
 import { Cart } from '../Cart/Cart'
 import { CardProduct } from './CardProduct'
 import { useCart } from '../../hooks/useCart'
+import { AddToCartIcon, RemoveFromCartIcon } from '../Icons/Icons'
 
 export function ProductsDetails ({ products }) {
-  const { previousPrice } = useCart()
+  const { previousPrice, checkProductInCart, removeFromCart, addToCart } = useCart()
   const { productTitle } = useParams()
   const cleanedProductTitle = productTitle.substring(1)
   const productInView = products.find(product => product.title === cleanedProductTitle)
   if(!productInView) return <h1>Product not found...</h1>
+
+  const isProductInCart = checkProductInCart(productInView)
   
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -26,8 +29,24 @@ export function ProductsDetails ({ products }) {
       <article className="product-article">
         <section className='cells'>
           <div className="product-title">
+            
             <h1>{productInView.title}</h1>
-            <h2>${previousPrice(productInView.discountPercentage, productInView.price)} <span>{productInView.discountPercentage}% OFF</span> <del>${productInView.price}</del></h2>
+              
+            
+            <div className='product-title-row'>
+              <h2>${previousPrice(productInView.discountPercentage, productInView.price)} <span>{productInView.discountPercentage}% OFF</span> <del>${productInView.price}</del></h2>
+              <button style={{backgroundColor: isProductInCart ? 'red' : '#09f'}} onClick={() => {
+                isProductInCart
+                  ? removeFromCart(productInView)
+                  : addToCart(productInView)
+              }}>
+                {
+                  isProductInCart
+                    ? <RemoveFromCartIcon />
+                    : <AddToCartIcon />
+                }
+              </button>
+            </div>
             <h3>Rating {productInView.rating}</h3>
             <p>Stock: {productInView.stock} units</p>
           </div>
