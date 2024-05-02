a015114b53994516de274cabb439162c85ff80dc
Jorge Ferreyra <jorxfer@gmail.com>
2024-05-02 14:08:35 -0300

    Responsive ProductsPage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Filters } from '../Filters/Filters.jsx'
 // eslint-disable-next-line react/prop-types
 export function Header () {
   return (
-    <header>
+    <header style={{width: '100%'}}>
       <Filters />
     </header>
   )
