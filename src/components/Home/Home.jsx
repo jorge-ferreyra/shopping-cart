ea06982bff1857772cd2484fc41eb383cf686022
Jorge Ferreyra <jorxfer@gmail.com>
2024-05-02 13:34:31 -0300

    Remove fetch

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,16 +3,8 @@ import { Products } from '../Products/Products.jsx'
 import { Footer } from '../Footer/Footer.jsx'
 import { TopMenu } from '../TopMenu/TopMenu.jsx'
 import { Cart } from '../Cart/Cart.jsx'
-import { useEffect } from 'react'
 
 export function Home () {
-  useEffect(() =>       {
-    fetch('https://dummyjson.com/products')
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-      })
-  }, [])
   return (
     <section className='main-section'>
       <TopMenu />
